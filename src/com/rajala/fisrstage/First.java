3159aa5e2020a6d217ef822a771e91cfa136cded
akbar615317 <akbarmca09@gmail.com>
2022-11-11 10:02:01 +0530

    "Increment decremt operators"

---
+++ src/com/rajala/fisrstage/First.java
@@ -0,0 +1,30 @@
+package com.rajala.fisrstage;
+
+public class First {
+
+    public static void main(String[] args) {
+    int a= 10;
+
+    int b = 20;
+
+        System.out.println(b);
+        System.out.println(b++); // 20
+
+
+        System.out.println(a+b); // 31
+
+        System.out.println(--b); // 20
+        System.out.println(b--); // 20
+
+
+        System.out.println(a); // 10
+        System.out.println(++a); //11
+        System.out.println(a);  //11
+        System.out.println(--b);  //10
+        System.out.println(a); //10
+    }
+
+}
+
+
+
